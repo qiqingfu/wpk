dd4b29f7143f7bf314e3958c8b9755a47bf03499
qiqf <723469354@qq.com>
2019-02-12 17:08:55 +0800

    feature ast compiler

--- bin/lib/Compiler.js
+++ bin/lib/Compiler.js
@@ -1,5 +1,10 @@
 const path = require('path')
 const fs = require('fs')
+const babylon = require('babylon')
+const traverse = require('@babel/traverse').default
+const generator = require('@babel/generator').default
+const types = require('@babel/types')
+
 
 /**
  * babylon 将源码转换成ast
@@ -22,7 +27,6 @@ class Compiler {
   run() {
     // 执行, 并创建模块的依赖关系 入口文件的绝对路径
     this.buildModule(path.resolve(this.root, this.entry), true)
-
     // 产出打包后的文件
     this.emitFile()
   }
@@ -34,7 +38,36 @@ class Compiler {
 
   // 解析 source 源码
   parse(source, parentPath) {
-    
+    // console.log(source)
+    // console.log(parentPath)
+    const ast = babylon.parse(source)
+    const dependencies = []  // 保存依赖的数组
+    const that = this
+
+    traverse(ast, {
+      CallExpression(p) {
+        let node = p.node
+        if (node.callee.name === 'require') {
+           // 改写 Identifier类型的name
+           node.callee.name = '__webpack_require__'
+           let moduleName = node.arguments[0].value
+
+           // 为 moduleName 检测后缀名
+           moduleName = moduleName + (path.extname(moduleName) ? '' : '.js')
+           moduleName = './' + path.join(parentPath, moduleName)
+           dependencies.push(moduleName)
+           
+           node.arguments = [types.stringLiteral(moduleName)]
+        }
+      }
+    })
+
+    // 将 ast数生成代码串
+    const sourceCode = generator(ast).code
+    return {
+      sourceCode,
+      dependencies
+    }
   }
 
   buildModule(modulePath, isEntry) {
@@ -51,8 +84,14 @@ class Compiler {
       sourceCode,
       dependencies
     } = this.parse(source, path.dirname(moduleName))
-
     this.modules[moduleName] = sourceCode
+
+    // 递归解析依赖项
+    if (dependencies.length > 0) {
+      dependencies.forEach(dep => {
+        this.buildModule(path.join(this.root, dep), false)
+      })
+    }
   }
 
   emitFile() {
